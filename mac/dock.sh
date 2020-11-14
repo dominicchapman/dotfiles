c243de1ec6492049294cc2f1658ecb82d19e9bc9
Dominic Chapman <dominicchapman@users.noreply.github.com>
2020-11-14 19:22:44 +0000

    chore: add dock preferences

---
+++ mac/dock.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+dockutil --no-restart --remove all
+dockutil --no-restart --add "/Applications/Google Chrome.app"
+dockutil --no-restart --add "/Applications/Slack.app"
+dockutil --no-restart --add "/Applications/zoom.us.app"
+dockutil --no-restart --add "/Applications/1Password 7.app"
+dockutil --no-restart --add "/Applications/Spotify.app"
+dockutil --no-restart --add "/Applications/iTerm.app"
+dockutil --no-restart --add "/Applications/Visual Studio Code.app"
+dockutil --no-restart --add "/Applications/Linear.app"
+dockutil --no-restart --add "/Applications/GitHub Desktop.app"
+
+killall Dock
